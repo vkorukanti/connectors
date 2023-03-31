3df4e880f88d6b025c0a1044ba37049fb0f7c1c6
Scott Sandre <scott.sandre@databricks.com>
2023-03-31 12:30:08 -0700

    Got basic partition pruning using AND(part1, part2)

--- kernel-default/src/test/scala/io/delta/core/internal/TableSuite.scala
+++ kernel-default/src/test/scala/io/delta/core/internal/TableSuite.scala
@@ -19,7 +19,7 @@ package io.delta.core.internal
 import scala.collection.JavaConverters._
 
 import io.delta.core.Table
-import io.delta.core.expressions.{EqualTo, Literal}
+import io.delta.core.expressions.{And, EqualTo, Literal}
 import io.delta.core.helpers.DefaultTableHelper
 import io.delta.core.util.GoldenTableUtils
 import org.scalatest.funsuite.AnyFunSuite
@@ -77,18 +77,34 @@ class TableSuite extends AnyFunSuite with GoldenTableUtils {
     }
   }
 
-  test("can perform partition pruning- basic - no checkpoint") {
+  test("can perform partition pruning - basic - no checkpoint") {
     withGoldenTable("basic-partitioned-no-checkpoint") { path =>
       val table = Table.forPath(path, new DefaultTableHelper())
       val snapshot = table.getLatestSnapshot.asInstanceOf[SnapshotImpl]
       val schema = snapshot.getSchema
-      val partitionFilter = new EqualTo(schema.column("part_a"), Literal.of(0L));
-      val scan = snapshot.getScanBuilder().withFilter(partitionFilter).build()
-      scan
+
+      val partitionFilter1 = new EqualTo(schema.column("part_a"), Literal.of(0L));
+      val scan1 = snapshot.getScanBuilder().withFilter(partitionFilter1).build()
+      scan1
         .getTasks
         .asScala
         .map(task => task.asInstanceOf[ScanTaskImpl].getAddFile)
         .foreach(add => assert(add.getPartitionValues.get("part_a").toLong == 0))
+
+      val partitionFilter2 = new And(
+        new EqualTo(schema.column("part_a"), Literal.of(0L)),
+        new EqualTo(schema.column("part_b"), Literal.of(0L))
+      )
+      val scan2 = snapshot.getScanBuilder().withFilter(partitionFilter2).build()
+      scan2
+        .getTasks
+        .asScala
+        .map(task => task.asInstanceOf[ScanTaskImpl].getAddFile)
+        .foreach { add => assert(
+            add.getPartitionValues.get("part_a").toLong == 0 &&
+            add.getPartitionValues.get("part_b").toLong == 0
+          )
+        }
     }
   }
 }
--- kernel/src/main/java/io/delta/core/internal/util/PartitionUtils.java
+++ kernel/src/main/java/io/delta/core/internal/util/PartitionUtils.java
@@ -1,11 +1,12 @@
 package io.delta.core.internal.util;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import io.delta.core.expressions.And;
 import io.delta.core.expressions.Expression;
+import io.delta.core.internal.lang.ListUtils;
 import io.delta.core.internal.lang.Tuple2;
 import io.delta.core.types.StructType;
 
@@ -33,17 +34,50 @@ public class PartitionUtils {
     public static Tuple2<Optional<Expression>, Optional<Expression>> splitMetadataAndDataPredicates(
             Expression condition,
             List<String> partitionColumns) {
-        return new Tuple2<>(Optional.of(condition), Optional.empty());
+        final Tuple2<List<Expression>, List<Expression>> metadataAndDataPredicates = ListUtils
+            .partition(
+                splitConjunctivePredicates(condition),
+                c -> isPredicateMetadataOnly(c, partitionColumns)
+            );
+
+        final Optional<Expression> metadataConjunction;
+        if (metadataAndDataPredicates._1.isEmpty()) {
+            metadataConjunction = Optional.empty();
+        } else {
+            metadataConjunction = Optional.of(And.apply(metadataAndDataPredicates._1));
+        }
+
+        final Optional<Expression> dataConjunction;
+        if (metadataAndDataPredicates._2.isEmpty()) {
+            dataConjunction = Optional.empty();
+        } else {
+            dataConjunction = Optional.of(And.apply(metadataAndDataPredicates._2));
+        }
+        return new Tuple2<>(metadataConjunction, dataConjunction);
     }
 
     private static List<Expression> splitConjunctivePredicates(Expression condition) {
-        return null;
+        if (condition instanceof And) {
+            final And andExpr = (And) condition;
+            return Stream.concat(
+                splitConjunctivePredicates(andExpr.getLeft()).stream(),
+                splitConjunctivePredicates(andExpr.getRight()).stream()
+            ).collect(Collectors.toList());
+        }
+        return Collections.singletonList(condition);
     }
 
-//    private def splitConjunctivePredicates(condition: Expression): Seq[Expression] = {
-//        condition match {
-//            case a: And => splitConjunctivePredicates(a.getLeft) ++ splitConjunctivePredicates(a.getRight)
-//            case other => other :: Nil
-//        }
-//    }
+    private static boolean isPredicateMetadataOnly(
+            Expression condition,
+            List<String> partitionColumns) {
+        Set<String> lowercasePartCols = partitionColumns
+            .stream().map(s -> s.toLowerCase(Locale.ROOT))
+            .collect(Collectors.toSet());
+
+        return condition
+            .references()
+            .stream()
+            .map(s -> s.toLowerCase(Locale.ROOT))
+            .allMatch(lowercasePartCols::contains);
+    }
 }
